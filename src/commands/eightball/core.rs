c974569566827c98026c37be90df60745143a995
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-25 15:16:58 -0500

    moved macro to its own module

--- src/commands/eightball/core.rs
+++ src/commands/eightball/core.rs
@@ -4,32 +4,8 @@ use rand::{
 };
 use std::fmt;
 
-macro_rules! create_answer_consts {
-    (
-        affirmative {
-            $($text:literal $($weight:literal)?),+
-        }
-
-        non_committal {
-            $($text2:literal $($weight2:literal)?),+
-        }
-
-        negative {
-            $($text3:literal $($weight3:literal)?),+
-        }
-    ) => {
-        macro_rules! weight {
-            ($value:literal) => { $value };
-            () => { 1.0 }
-        }
-
-        pub const ANSWERS: Answers = Answers(&[
-            $(Answer { tone: AnswerTone::Affirmative, text: $text, weight: weight!($($weight)?)},)+
-            $(Answer { tone: AnswerTone::NonCommittal, text: $text2, weight: weight!($($weight2)?)} ,)+
-            $(Answer { tone: AnswerTone::Negative, text: $text3, weight: weight!($($weight3)?)} ,)+
-        ]);
-    }
-}
+#[macro_use]
+mod macros;
 
 create_answer_consts! {
     affirmative {
