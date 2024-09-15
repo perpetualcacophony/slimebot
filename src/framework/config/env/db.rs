8027dd5be88c3c5ddc4768d7318ea96441f499fe
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-09-15 09:06:45 -0500

    fixed serialization of mongodb url

--- src/framework/config/env/db.rs
+++ src/framework/config/env/db.rs
@@ -1,29 +1,39 @@
-use mongodb::options::ServerAddress;
-
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
-#[serde(from = "Partial")]
 pub struct DbEnvironment {
-    url: ServerAddress,
+    #[serde(default)]
+    url: Url,
 }
 
 impl DbEnvironment {
-    pub fn url(&self) -> &ServerAddress {
-        &self.url
+    pub fn url(&self) -> &mongodb::options::ServerAddress {
+        &self.url.inner
     }
 }
 
-impl From<Partial> for DbEnvironment {
-    fn from(partial: Partial) -> Self {
-        Self {
-            url: partial.url.unwrap_or_else(|| ServerAddress::Tcp {
-                host: "db".to_owned(),
-                port: Some(27017),
-            }),
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[serde(from = "mongodb::options::ServerAddress", into = "String")]
+struct Url {
+    inner: mongodb::options::ServerAddress,
+}
+
+impl Default for Url {
+    fn default() -> Self {
+        mongodb::options::ServerAddress::Tcp {
+            host: "db".to_owned(),
+            port: Some(27017),
         }
+        .into()
     }
 }
 
-#[derive(serde::Deserialize)]
-struct Partial {
-    url: Option<ServerAddress>,
+impl From<mongodb::options::ServerAddress> for Url {
+    fn from(value: mongodb::options::ServerAddress) -> Self {
+        Self { inner: value }
+    }
+}
+
+impl From<Url> for String {
+    fn from(value: Url) -> Self {
+        value.inner.to_string()
+    }
 }
